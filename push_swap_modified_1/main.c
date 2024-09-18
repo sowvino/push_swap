5ce648c8645b47180c70723eed80876fa2d0e2b4
sowmya <106254533+sowvino@users.noreply.github.com>
2024-09-18 15:57:36 +0200

    Add files via upload

---
+++ push_swap_modified_1/main.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: selango <selango@student.42.fr>            #+#  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024-09-18 10:35:07 by selango           #+#    #+#             */
+/*   Updated: 2024-09-18 10:35:07 by selango          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+int	main(int argc, char **argv)
+{
+	t_stack_node	*a;
+	t_stack_node	*b;
+
+	a = NULL;
+	b = NULL;
+	if (1 == argc || (2 == argc && !argv[1][0]))
+		return (1);
+	else if (2 == argc)
+		argv = ft_split(argv[1], ' ');
+	stack_init(&a, argv + 1, 2 == argc);
+	if (!stack_sorted(a))
+	{
+		if (stack_len(a) == 2)
+			sa(&a, false);
+		else if (stack_len(a) == 3)
+			handle_three(&a);
+		else
+			push_swap(&a, &b);
+	}
+	free_stack(&a);
+}
